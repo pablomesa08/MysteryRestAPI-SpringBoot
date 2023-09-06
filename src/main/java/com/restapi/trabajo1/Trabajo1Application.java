ce939c49b61ebf8103e56c5da8527e6b206244c2
pablomesa08 <pablo.mesa71@eia.edu.co>
2023-09-06 15:31:36 -0500

    Main Configuration

---
+++ src/main/java/com/restapi/trabajo1/Trabajo1Application.java
@@ -0,0 +1,13 @@
+package com.restapi.trabajo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabajo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabajo1Application.class, args);
+    }
+
+}
